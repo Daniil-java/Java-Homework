cf50ccd8cce90b79c4e3109af3cc3a99775cbaa8
Daniil <saponembigas@gmail.com>
2021-04-24 21:50:12 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/lesson1/HomeWorkApp.java
@@ -0,0 +1,40 @@
+package ru.geekbrains.lesson1;
+
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = -5;
+        int b = 2;
+        System.out.println( (a + b >= 0) ? "Сумма положительная" : "Сумма отрицательная");
+    }
+
+    public static void printColor() {
+        int value = 101;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Жёлтый");
+        } else {
+            System.out.println("Зелёный");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 5;
+        int b = 2;
+        System.out.println(a >= b ? "a >= b" : "a < b");
+    }
+}
